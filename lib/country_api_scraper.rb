d8c91222bab416a510d6fd337f59d2951a3c9180
jinook929 <jinook929@gmail.com>
2021-02-02 21:36:03 -0500

    create self.get_country_details method in CountryApiScraper class

--- lib/country_api_scraper.rb
+++ lib/country_api_scraper.rb
@@ -23,4 +23,32 @@ class CountryApiScraper
             }
         }
     end
+
+    # Get detailed individual country info based on given country code by scraping
+    def self.get_country_details(code)
+        hash = {}
+        # Request error check with a web page to scrape
+        begin  # when the web page works
+            # Get the country page info based on code argument
+            doc = Nokogiri::HTML(open("http://www.geognos.com/geo/en/cc/#{code}.html"))
+            # Fill in the returning hash
+            hash[:location] = doc.css("#Location p").text.chop
+            if hash[:capital] == "N/A"
+                hash[:capital] = doc.css("#nm_cont_Capital td")[1].text.strip
+            end
+            hash[:language] = doc.css("#nm_cont_Languages > table > tbody > tr > td")[0].text.strip
+            hash[:population] = doc.css("#Population p").text.gsub(/\302\240/, ",").chop
+            hash[:currency] = "#{doc.css("#Currencycode p").text.gsub(/[[:space:]]/, "")} (#{doc.css("#Currencyname p").text.gsub(160.chr("UTF-8"), "")})"
+            hash[:background] = doc.css("#Background p").text.strip.chop
+        rescue # when the web page does not work
+            hash[:location] = "N/A"
+            hash[:capital] = "N/A"
+            hash[:language] = "N/A"
+            hash[:population] = "N/A"
+            hash[:currency] = "N/A"
+            hash[:background] = "'More Info' link below will not work..."
+        end
+        # Return country info hash
+        hash
+    end
 end
