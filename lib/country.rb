5d1b013b8aea4eaec71cf8cba3125ae6d77d9b49
jinook929 <jinook929@gmail.com>
2021-02-02 21:54:16 -0500

    create initialize method from given hash for Country class

--- lib/country.rb
+++ lib/country.rb
@@ -6,4 +6,15 @@ class Country
     def self.all
         @@all
     end
+
+    # Initialize a new instance from given hash
+    def initialize(hash)
+        hash.each {|k, v|
+            # Create getters and setters
+            self.class.attr_accessor(k)
+            # Set value for created variable
+            self.send("#{k}=", v)
+        }
+        self.all.push(self)
+    end
 end
