74482bc324950b8ebd412630966e13b1f37addd5
jinook929 <jinook929@gmail.com>
2021-02-03 12:11:08 -0500

    refactor environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,4 @@ require "json"
 require "colorize"
 
 # Require all .rb files in lib folder
-# binding.pry
-# Dir['./lib/*.rb'].each {|file| require_relative '.#{file}'}
-require_relative '../lib/cli'
-require_relative '../lib/country'
-require_relative '../lib/weather'
-require_relative '../lib/country_api_scraper'
-require_relative '../lib/weather_scraper'
+Dir['./lib/*.rb'].each {|file| require_relative ".#{file}"}
