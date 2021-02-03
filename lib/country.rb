cc3b79f2a574a3bf46332c2847a68462da83ec77
jinook929 <jinook929@gmail.com>
2021-02-02 21:46:05 -0500

    create class variable to collect the Country instances

--- lib/country.rb
+++ lib/country.rb
@@ -1,3 +1,9 @@
+# To create Country instances
 class Country
-
+    # Set class variable to collect the instances
+    @@all = []
+    # Get @@all value
+    def self.all
+        @@all
+    end
 end
