269398c14509726e5e54ffd46245ff27f1da0a0b
jinook929 <jinook929@gmail.com>
2021-02-02 22:12:07 -0500

    create initialize method from given hash for Weather class

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,12 @@
+# To create Weather instances
 class Weather
-
+    # Initialize a new instance from given hash
+    def initialize(hash)
+        hash.each {|k, v|
+            # Create getters and setters
+            self.class.attr_accessor(k)
+            # Set value for created variable
+            self.send("#{k}=", v)
+        }
+    end
 end
