f0822ca7adf038f69855e0e71cd26c4c5ef00040
jinook929 <jinook929@gmail.com>
2021-02-10 18:26:30 -0500

    improve repl.it version with README revision

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ require "open-uri"
 require "json"
 require "colorize"
 require "csv"
+# require 'geocoder' <= for repl.it version
 
 # Require all .rb files in lib folder
 Dir["./lib/*.rb"].each { |file| require_relative ".#{file}" }
